1fcda976bd976becd6fc88a933521c1027d0cc0f
m4rceee <91464427+m4rceee@users.noreply.github.com>
2023-06-13 13:38:10 +0800

    added php

--- admin-teacher.php
+++ admin-teacher.php
@@ -1,3 +1,61 @@
+<?php
+
+$status = $emptyField = "";
+
+  $con = mysqli_connect("localhost", "root", "", "teachers_db");
+
+  if(!$con) {
+    die("Connection Failed: ". mysqli_connect_error());
+  }
+
+  if(isset($_POST['save_teacher'])) {
+
+    $fieldsToValidate = array('fullname', 'gender', 'birthdate', 'city', 'department', 'contact', 'email');
+    $allFieldsNotEmpty = true;
+
+    foreach ($fieldsToValidate as $field) {
+      if (empty($_POST[$field])) {
+          $allFieldsNotEmpty = false;
+          $emptyField = "<div class='alert alert-danger mt-2'><strong>Missing field/s.</strong></div>";
+          break; // Stop the loop as soon as an empty field is found
+      }
+    }
+
+    if ($allFieldsNotEmpty) {
+
+        $fullname = mysqli_real_escape_string($con, $_POST['fullname']);
+        $gender = mysqli_real_escape_string($con, $_POST['gender']);
+        $birthdate = mysqli_real_escape_string($con, $_POST['birthdate']);
+        $city = mysqli_real_escape_string($con, $_POST['city']);
+        $department = mysqli_real_escape_string($con, $_POST['department']);
+        $contact = mysqli_real_escape_string($con, $_POST['contact']);
+        $email = mysqli_real_escape_string($con, $_POST['email']);
+
+        $query = "INSERT INTO teachers (full_name, gender, birthdate, city, department, contact, email) 
+                  VALUES ('$fullname', '$gender', '$birthdate', '$city', '$department', '$contact', '$email')";
+        
+        $query_run = mysqli_query($con, $query);
+
+        if($query_run) {
+          header("Location: admin-teacher.php");
+          $status = "<div class='alert alert-success alert-dismissible fade show mt-2'><strong>Teacher created successfully.</strong></div>";
+          exit();
+        } else {
+          $status = "<div class='alert alert-warning alert-dismissible fade show mt-2'><strong>Teacher creation unsuccessful. Please try again.</strong></div>";
+        }
+    }
+  }
+?>
+
+<?php
+  $con = mysqli_connect("localhost", "root", "", "teachers_db");
+
+  if(!$con) {
+    die("Connection Failed: ". mysqli_connect_error());
+  }
+?>
+
+
 <!DOCTYPE html>
   <html lang="en">
   <head>
@@ -48,38 +106,40 @@
                   <div class="card-body">
                       <div class="card-title">
                           <h1>REGISTER:</h1>
+                          <?php echo $status; ?>
+                          <?php echo $emptyField; ?>
                       </div>
                       <form action="admin-teacher.php" method="POST">
                           <div class="mb-3 mt-3">
                               <label for="fullname" class="form-label">Full Name:</label>
-                              <input type="text" class="form-control" id="fullname" placeholder="Enter full name" name="fullname" autocomplete="off" required> 
+                              <input type="text" class="form-control" id="fullname" placeholder="Enter full name" name="fullname" autocomplete="off"> 
                             </div>
                             <div class="mb-3">
                               <label for="gender" class="form-label">Gender:</label>
-                              <input type="text" class="form-control" id="gender" placeholder="Enter gender" name="gender" autocomplete="off" required>
+                              <input type="text" class="form-control" id="gender" placeholder="Enter gender" name="gender" autocomplete="off">
                             </div>
                             <div class="mb-3">
                               <label for="birthdate" class="form-label">Date of Birth:</label>
-                              <input type="date" class="form-control" id="birthdate" placeholder="Enter birth date" name="birthdate" autocomplete="off" required>
+                              <input type="date" class="form-control" id="birthdate" placeholder="Enter birth date" name="birthdate" autocomplete="off">
                             </div>
                             <div class="mb-3">
                               <label for="city" class="form-label">City:</label>
-                              <input type="text" class="form-control" id="city" placeholder="Enter city" name="city" autocomplete="off" required>
+                              <input type="text" class="form-control" id="city" placeholder="Enter city" name="city" autocomplete="off">
                             </div>
                             <div class="mb-3">
                               <label for="department" class="form-label">Department:</label>
-                              <input type="text" class="form-control" id="department" placeholder="Enter department" name="department" autocomplete="off" required>
+                              <input type="text" class="form-control" id="department" placeholder="Enter department" name="department" autocomplete="off">
                             </div>
                             <div class="mb-3">
                               <label for="contact" class="form-label">Contact Number:</label>
-                              <input type="text" class="form-control" id="contact" placeholder="Enter contact number" name="contact" autocomplete="off" required>
+                              <input type="text" class="form-control" id="contact" placeholder="Enter contact number" name="contact" autocomplete="off">
                             </div>
                             <div class="mb-3">
                               <label for="email" class="form-label">E-mail:</label>
-                              <input type="email" class="form-control" id="email" placeholder="Enter e-mail" name="email" autocomplete="off" required>
+                              <input type="email" class="form-control" id="email" placeholder="Enter e-mail" name="email" autocomplete="off">
                             </div>
                             <div class="d-flex justify-content-center gap-2 mt-4 mb-2">
-                              <button type="submit" id="teachersbmt" class="btn">Log In</button>
+                              <button type="submit" name="save_teacher" id="teachersbmt" class="btn">Log In</button>
                               <a class="btn text-white" href="#" role="button" id="cancel">Cancel</a>
                             </div>
                             
@@ -95,7 +155,7 @@
                       <div class="card-title">
                           <h1 style="color: #004500;">TEACHERS:</h1>
                       </div>
-                      <table class="table table-striped" id="table-bg">
+                      <table class="table table-bordered table-striped" id="table-bg">
                           <thead>
                             <tr>
                               <th>EMP</th>
@@ -105,36 +165,29 @@
                             </tr>
                           </thead>
                           <tbody>
-                            <tr>
-                              <td>0001</td>
-                              <td>Kahit Sino</td>
-                              <td>Information Technology</td>
-                              <td>
-                                  <button class="btn btn-primary">Edit</button>
-                                  <button class="btn btn-danger">Delete</button>
+                            <?php
 
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>0002</td>
-                              <td>Kahit Sino</td>
-                              <td>Information Technology</td>
-                              <td>
-                                  <button class="btn btn-primary">Edit</button>
-                                  <button class="btn btn-danger">Delete</button>
+                              $query = "SELECT * FROM teachers";
+                              $query_run = mysqli_query($con, $query);
 
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>0003</td>
-                              <td>Kahit Sino</td>
-                              <td>Information Technology</td>
-                              <td>
-                                  <button class="btn btn-primary">Edit</button>
-                                  <button class="btn btn-danger">Delete</button>
-
-                              </td>
-                            </tr>
+                              if(mysqli_num_rows($query_run) > 0) {
+                                foreach($query_run as $teacher) {
+                                  ?>
+                                    <tr>
+                                      <td><?= $teacher['EMP'] ?></td>
+                                      <td><?= $teacher['full_name'] ?></td>
+                                      <td><?= $teacher['department'] ?></td>
+                                      <td>
+                                        <a href="#" class="btn btn-primary btn-sm">Edit</a>
+                                        <a href="#" class="btn btn-danger btn-sm">Delete</a>
+                                      </td> 
+                                    </tr>
+                                  <?php
+                                }
+                              } else {
+                                echo "No records found.";
+                              }
+                            ?>
                           </tbody>
                         </table>
                   </div>
